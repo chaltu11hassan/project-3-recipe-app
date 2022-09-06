d41e8eefcc6740002ae668c6e2ebdd4f1a88f8b8
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-09-05 23:05:51 -0500

    initial recommit

---
+++ src/components/homeComponents/HomeScreen.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import React, {useEffect, useState} from 'react'
+import AdBanner from './AdBanner'
+
+const HomeScreen = () => {  
+
+  const [recipes, setRecipes] = useState([])
+
+  const url = 'https://recipes.devmountain.com'
+
+  const getRecipes =()=>{
+    axios.get(`${url}/recipes`).then((res)=>{setRecipes(res.data)
+    console.log(res.data)
+  })
+  }
+  useEffect(()=>{
+    getRecipes();
+  },[])
+
+  return (
+    <div>
+      <AdBanner />
+      {/* Much code from Part 2 will be placed around here. Do your best! */}
+    </div>
+  )
+}
+
+export default HomeScreen;

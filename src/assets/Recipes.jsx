e84977542ec324dee9cb64713457f701324a3133
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-09-05 23:49:23 -0500

    more css styling added

---
+++ src/assets/Recipes.jsx
@@ -0,0 +1,7 @@
+const Recipes = [
+    {
+
+    }
+]
+
+export default Recipes;
--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -30,16 +30,12 @@ const NewRecipeScreen = () => {
   const onSubmit = (values) => {
     values.ingredients = ingredients;
     console.log(values);
+
+    // axios
+    // .post(`https://recipes.devmountain.com/recipes`, values)
   };
-  //   axios
-  //     .post(`https://recipes.devmountain.com/recipes`, values)
-  //     .then((res) => {
-  //       console.log(res.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
+
+  
 
   const ingredientDisplay = ingredients.map((ing) => {
     return (

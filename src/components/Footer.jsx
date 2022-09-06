df90aa783f28e1ef44435f83b04a3713181de7f7
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-09-06 00:50:04 -0500

    css updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,28 @@
 import React from "react";
-import { ImFacebook, ImInstagram, ImTwitter, ImGithub } from "react-icons/im";
-
 import styles from "./Header.module.css";
+import { ImFacebook, ImInstagram, ImTwitter, ImGithub } from "react-icons/im";
 
 const Footer = () => {
   return (
     <footer>
-      <div className={styles.socials_container}>
+      <div className={styles.social_container}>
         <h5>Hit us up on our socials!</h5>
         <div className={styles.icons}>
-          <a href="https://www.devmountain.com" target="_blank" className={styles.cirlce}>
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImFacebook size="1.5em" color="#3f3f3f" />
           </a>
-          <a href="https://www.devmountain.com" target="_blank" className={styles.cirlce}>
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImInstagram size="1.5em" color="#3f3f3f" />
           </a>
-          <a href="https://www.devmountain.com" target="_blank" className={styles.cirlce}>
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImTwitter size="1.5em" color="#3f3f3f" />
           </a>
-          <a href="https://www.devmountain.com" target="_blank" className={styles.cirlce}>
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImGithub size="1.5em" color="#3f3f3f" />
           </a>
         </div>
       </div>
+      {/* <div> */}
       <svg
         width="110"
         height="110"
@@ -39,6 +39,7 @@ const Footer = () => {
           stroke-linejoin="round"
         />
       </svg>
+      {/* </div> */}
     </footer>
   );
 };
